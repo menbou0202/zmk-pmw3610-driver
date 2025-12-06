c11a58637da7d6a0800dd5f131772efcaea3c61d
cordwisteria <cordwisteria@users.noreply.github.com>
2025-12-06 22:47:30 +0900

    Fixed compile error due to updating zmk_keymap_layer_activate() argument for layer lock by zmkfirmware/zmk#2717.

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -549,7 +549,7 @@ static bool automouse_triggered = false;
 
 static void activate_automouse_layer() {
     automouse_triggered = true;
-    zmk_keymap_layer_activate(AUTOMOUSE_LAYER);
+    zmk_keymap_layer_activate(AUTOMOUSE_LAYER, false);
     k_timer_start(&automouse_layer_timer, K_MSEC(CONFIG_PMW3610_AUTOMOUSE_TIMEOUT_MS), K_NO_WAIT);
 }
 
@@ -812,7 +812,7 @@ static int pmw3610_init(const struct device *dev) {
     // 🆕 起動時にデフォルトレイヤーを適用
     uint8_t default_layer = CONFIG_NAPE_DEFAULT_LAYER;
     LOG_INF("Setting default layer to %d", default_layer);
-    zmk_keymap_layer_activate(default_layer);
+    zmk_keymap_layer_activate(default_layer, false);
 
     // 🔹 ここで last_orientation_layer を初期化
     last_orientation_layer = default_layer;
